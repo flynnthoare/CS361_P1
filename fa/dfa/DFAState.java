b55092d0ddb753fcac108c2f5aae78255d8b2bb6
NickDBortz <NicholasBortz@u.boisestate.edu>
2025-02-11 10:54:20 -0700

    Added isFinal Flag w/ Overload Constructor

--- fa/dfa/DFAState.java
+++ fa/dfa/DFAState.java
@@ -4,4 +4,13 @@ import fa.State;
 
 public class DFAState extends State {
     // TODO: Setup the flags and name for this state with a child constructor
+    private boolean isFinal;
+
+    public DFAState(String name) {
+        super(name);
+    }
+
+    public void makeFinal() {
+        isFinal = true;
+    }
 }
