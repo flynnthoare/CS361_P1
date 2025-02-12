0c673422a543e5a1bc6a5bb714e8af9c5722c63f
Flynn <flynnhoare@u.boisestate.edu>
2025-02-11 17:15:40 -0700

    Added swap, addTransition, isStart, isFinal. Need testing

--- fa/dfa/DFA.java
+++ fa/dfa/DFA.java
@@ -10,7 +10,7 @@ public class DFA implements DFAInterface {
     // sigma includes the total alphabet
     private Set<Character> sigma = new HashSet<>();
     // transitionTable maps from a state name to the list of state names that occur depending on the transition
-    private Map<String, ArrayList<String>> transitionTable = new HashMap<>();
+    private Map<String, String> transitionTable = new HashMap<>();
 
     // Store current state and start state
     private DFAState start;
@@ -90,21 +90,77 @@ public class DFA implements DFAInterface {
 
     @Override
     public boolean isFinal(String name) {
-        return false;
+        DFAState state = getState(name);
+        if (state.isFinal()) {
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 
     @Override
     public boolean isStart(String name) {
-        return false;
+        DFAState state = getState(name);
+        if (state.isStart()) {
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 
+    //Might need to add additional checks to see if a transition already exists
     @Override
     public boolean addTransition(String fromState, String toState, char onSymb) {
-        return false;
+        if (getState(fromState) == null || getState(toState) == null || !sigma.contains(onSymb)) {
+            return false;
+        }
+
+        String key = fromState + "," + onSymb;
+        transitionTable.put(key, toState);
+        
+        
+        return true;
     }
 
     @Override
     public DFA swap(char symb1, char symb2) {
-        return null;
+        DFA swappedDFA = new DFA();
+        
+        // copy alphabet
+        for (char c : this.sigma) {
+            swappedDFA.addSigma(c);
+        }
+
+        //Copy States
+        for (DFAState state : this.states) {
+            swappedDFA.addState(state.getName());
+
+            if (state.isFinal()) {
+                swappedDFA.setFinal(state.getName());
+            }
+            if (state.isStart()) {
+                swappedDFA.setStart(state.getName());
+            }
+        }
+
+        // copy transitions with symbol swaps
+        for (Map.Entry<String, String> transition :this.transitionTable.entrySet()) {
+            String key = transition.getKey();
+            String toState = transition.getValue();
+
+            String[] keyParts = key.split(",");
+            String fromState = keyParts[0];
+            char symbol = keyParts[1].charAt(0);
+
+            char newSymbol = (symbol == symb1) ? symb2 : (symbol == symb2) ? symb1: symbol;
+
+            swappedDFA.addTransition(fromState, toState, newSymbol);
+        }
+
+        return swappedDFA;
+
+        
     }
 }
